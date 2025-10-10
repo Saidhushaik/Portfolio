f2ed19d5c2c5d87846ae8fdf59d6dda5567c215d
Saidhu Shaik <hussenbasha6262@gmail.com>
2025-10-09 23:11:52 -0400

    Update my details

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 // Initialize AOS animations
 AOS.init({
-  duration: 800, // Set animation duration
-  easing: 'ease-in-out', // Set animation easing type
-  once: true, // Trigger animation once when it comes into view
+  duration: 800,
+  easing: 'ease-in-out',
+  once: true,
 });
 
 // Smooth scroll for nav links
 document.querySelectorAll('nav a').forEach(anchor => {
   anchor.addEventListener('click', e => {
     e.preventDefault();
-    
-    // Smoothly scroll to the targeted section
     document.querySelector(anchor.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
   });
-});
-
-// Optional: Add an event listener for resize if you want to handle specific changes dynamically (for example, handling sticky navbars).
-window.addEventListener('resize', function() {
-  // Handle window resize actions here (if needed)
 });
